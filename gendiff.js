03a6184ebf553c16454563844f8979378cdccda2
Alexey Vorobev <35743408+w0rm76@users.noreply.github.com>
2025-03-11 17:22:11 +0100

    add option and arguments

--- gendiff.js
+++ gendiff.js
@@ -10,6 +10,13 @@ program
     .version('0.0.1');
 
 program
+    .option('-f, --format <type>', 'output format');
+
+program
+    .argument('<filepath1>', 'filepath1')
+    .argument('<filepath2>', 'filepath2');
+
+program
     .description('Compares two configuration files and shows a difference.');
 
 program.parse(process.argv);
